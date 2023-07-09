db6f5e207c52bf951ba3405d465fb4f864bc43b3
Ivana305 <ikapaprika011@gmail.com>
2023-07-09 16:42:09 +0200

    the feature planet1 is added and working fine

--- src/Main.java
+++ src/Main.java
@@ -12,6 +12,12 @@ public class Main {
         AdamX.temp="12344321 celcius";
         AdamX.size="32942Km";
 
+        Planet1 xeon= new Planet1();
+        xeon.name="reon";
+        xeon.color="pink";
+        xeon.temp="-45celcius";
+        xeon.size="1cm";
+
     }
 
 }
